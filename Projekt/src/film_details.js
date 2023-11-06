771fafa908c4c03a7f9477a07d67f2b5d4caeffe
PatrykPut <patryk.put@lise.de>
2023-11-06 09:35:18 +0100

    film_details

--- Projekt/src/film_details.js
+++ Projekt/src/film_details.js
@@ -1,27 +1,38 @@
+import styled from "styled-components"; 
 
+const Title = styled.h1`
+  color: red;
+  font-size: 75px;
+  text-align: center;
+  background-color: grey;
+  border-radius: 3px;
+  padding: 10x;
+  display: block;
+  font-family: Arial;
+`; 
 
-document.addEventListener("DOMContentLoaded", function () {
-
-    const movieTitle = "Movie Title aus der DB";
-    const movieDescription = "Movie Description aus der DB";
-
-    document.querySelector("h1").textContent = movieTitle;
-    document.querySelector("h2").textContent = movieDescription;
-
-
-    const filmPlakatSrc = "URL des Filmbildes aus der DB";
-    const filmPlakat = document.querySelector(".Film-Plakat");
-    filmPlakat.src = filmPlakatSrc;
-
-
-    const backButton = document.querySelector(".dropdown-content button:first-child");
-    backButton.addEventListener("click", function () {
-        
-    });
-
-
-    const homepageButton = document.querySelector(".dropdown-content button:last-child");
-    homepageButton.addEventListener("click", function () {
-
-    });
-});
+export const FilmDetails = () => {
+    return (
+      <>
+  <Title>Movie Title</Title>                                
+  <h2>Movie Description</h2>                          
+  <p>Beschreibung des Filmes. Lorem Ipsum <br/>        
+  Lorem Ipsum <br/>
+  Lorem Ipsum</p>
+ 
+  <img class="Film-Plakat" src="https://ih1.redbubble.net/image.3403824752.9898/fposter,small,wall_texture,square_product,600x600.jpg" alt="Film-Plakat von Django Unchained. Zu sehen sind Jamie Foxx, Christoph Waltz und Leonardo DiCaprio"/>
+  
+  <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+  <h3>Bewertung schreiben:</h3>
+  <textarea class="bewertungsbox" name="bewertungen" id="bewertungen" cols="60" rows="5"></textarea>
+  
+  <div class="dropdown">
+      <span><img widht="40" height="30" src="../public/backbutton.png" alt=""/></span>
+      <div class="dropdown-content">
+        <button>Go back</button>
+        <button>Go back to homepage</button>
+      </div>
+    </div>
+    </>
+    )
+}
