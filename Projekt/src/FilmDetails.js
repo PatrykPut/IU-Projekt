1cf90c563071c6bdf56528828da0761dda846e66
borchisan <lborchert97@gmail.com>
2023-11-07 23:40:18 +0100

    habe jetzt ein paar Sachen in der Details Seite gefixd aber bin noch nicht ganz durch

--- Projekt/src/FilmDetails.js
+++ Projekt/src/FilmDetails.js
@@ -13,10 +13,48 @@ const Title = styled.h1`
   font-family: Arial;
 `; 
 
-export const FilmDetails = () => {
+const DescriptionTitle = styled.h2`
+    color: blanchedalmond;
+    font-size: 50px;
+    text-align: center;
+    font-family: Arial;
+`;
+
+const RatingHeader = styled.h3`
+    text-align: right;
+    font-family: Arial;
+    font-size: 30px;
+`;
+
+const TextonSite = styled.p `
+    text-align: center;
+    font-family: Arial;
+`;
+
+const Plakat = styled.img`
+  width: 200px;
+  position: absolute;
+  top: 10px;
+  right: 20px;
+  border-radius: 10px;
+  opacity: 0.99;
+`;
+const ImageContainer = styled.div`
+  position: relative;
+  text-align: right;
+`;
+const Textarea = styled.textarea`
+    float: right;
+    border-radius: 5px;
+    
+`
+  
+  export const FilmDetails = () => {
 
   const { id } = useParams();
   const [film, setFilm] = useState([]);
+
+
 
   useEffect(() => {
   fetch(`http://localhost/IUProjekt/Projekt/src/FilmDetails.php?id=${id}`)
@@ -30,16 +68,24 @@ export const FilmDetails = () => {
     return (
       <>
   <Title>{film.name}</Title>                                
-  <h2>Movie Description</h2>                          
-  <p>{film.description}<br/>        
+  <DescriptionTitle>Movie Description</DescriptionTitle>                          
+  <TextonSite>{film.description}<br/>        
   Lorem Ipsum <br/>
-  Lorem Ipsum</p>
- 
-  <img class="Film-Plakat" src="https://ih1.redbubble.net/image.3403824752.9898/fposter,small,wall_texture,square_product,600x600.jpg" alt="Film-Plakat von Django Unchained. Zu sehen sind Jamie Foxx, Christoph Waltz und Leonardo DiCaprio"/>
+  Lorem Ipsum</TextonSite>
+
+
+       <ImageContainer>
+        <Plakat
+          src="https://ih1.redbubble.net/image.3403824752.9898/fposter,small,wall_texture,square_product,600x600.jpg"
+          alt="Film-Plakat von Django Unchained. Zu sehen sind Jamie Foxx, Christoph Waltz und Leonardo DiCaprio"
+        />
+      </ImageContainer>
+  
+
   
   <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-  <h3>Bewertung schreiben:</h3>
-  <textarea class="bewertungsbox" name="bewertungen" id="bewertungen" cols="60" rows="5"></textarea>
+  <RatingHeader>Bewertung schreiben:</RatingHeader>
+  <Textarea rows="5" cols="45"></Textarea>
   
   <div class="dropdown">
       <span><img width="40" height="30" src="../public/backbutton.png" alt=""/></span>
